a12183ef1118691c8094013146e1be7741ad964b
shiauweitsung <zz830527@gmail.com>
2023-03-10 17:04:30 +0800

    [change] add modal component

---
+++ src/components/Modal.tsx
@@ -0,0 +1,84 @@
+import React, { useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
+
+interface ModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const fadeOut = keyframes`
+  from {
+    opacity: 1;
+  }
+  to {
+    opacity: 0;
+  }
+`;
+
+const ModalOverlay = styled.div<{ isOpen: boolean }>`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: ${({ isOpen }) => (isOpen ? 'block' : 'none')};
+  animation: ${({ isOpen }) => (isOpen ? fadeIn : fadeOut)} 0.3s ease-in-out;
+  z-index: 999;
+`;
+
+const ModalWrapper = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: white;
+  border-radius: 10px;
+  padding: 1rem;
+  z-index: 1000;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  animation: ${fadeIn} 0.3s ease-in-out;
+`;
+
+const ModalContent = styled.div``;
+
+const Modal = ({ isOpen, onClose, children }: React.PropsWithChildren<ModalProps>) => {
+  const handleOutsideClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    const handleEscapeKey = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose();
+      }
+    };
+
+    window.addEventListener('keydown', handleEscapeKey);
+
+    return () => {
+      window.removeEventListener('keydown', handleEscapeKey);
+    };
+  }, [onClose]);
+
+  return (
+    <ModalOverlay isOpen={isOpen} onClick={handleOutsideClick}>
+      <ModalWrapper>
+        <ModalContent>{children}</ModalContent>
+      </ModalWrapper>
+    </ModalOverlay>
+  );
+};
+
+export default Modal;
